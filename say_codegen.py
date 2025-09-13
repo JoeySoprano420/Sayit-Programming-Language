8c171a02376feb6a2f1e9838bfa0c066a9cc3cbb
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2025-09-13 01:30:04 -0400

    Refactor Codegen class and update emit_print method
    Refactor emit_print method to use type hints and improve printf handling.

--- say_codegen.py
+++ say_codegen.py
@@ -3,46 +3,34 @@ from llvmlite import ir
 
 class Codegen:
     def __init__(self):
-        self.module = ir.Module("sayit")
-        self.func = None
-        self.builder = None
-
-    def emit_main(self):
-        fnty = ir.FunctionType(ir.IntType(32), [])
-        self.func = ir.Function(self.module, fnty, name="main")
-        block = self.func.append_basic_block(name="entry")
-        self.builder = ir.IRBuilder(block)
-
-    def emit_print(self, msg):
-        voidptr = ir.IntType(8).as_pointer()
-        printf_ty = ir.FunctionType(ir.IntType(32), [voidptr], var_arg=True)
-        printf = self.module.globals.get("printf") or \
-                 ir.Function(self.module, printf_ty, name="printf")
-        cstr = self.builder.global_string(msg, name="str")
-        self.builder.call(printf, [cstr])
-
-from llvmlite import ir
-
-class Codegen:
-    def __init__(self):
         self.module = ir.Module(name="sayit")
         self.func = None
         self.builder = None
 
     def emit_main(self):
+        """Create main() function and entry block"""
         fnty = ir.FunctionType(ir.IntType(32), [])
         self.func = ir.Function(self.module, fnty, name="main")
         block = self.func.append_basic_block(name="entry")
         self.builder = ir.IRBuilder(block)
 
-    def emit_print(self, text):
+    def emit_print(self, text: str):
+        """Emit a printf call with a string literal"""
         voidptr = ir.IntType(8).as_pointer()
         printf_ty = ir.FunctionType(ir.IntType(32), [voidptr], var_arg=True)
-        printf = self.module.globals.get("printf") or \
-                 ir.Function(self.module, printf_ty, name="printf")
-        cstr = self.builder.global_string_ptr(text)
+
+        # Reuse or create printf declaration
+        if "printf" in self.module.globals:
+            printf = self.module.globals["printf"]
+        else:
+            printf = ir.Function(self.module, printf_ty, name="printf")
+
+        # Create global string pointer
+        cstr = self.builder.global_string_ptr(text, name="str")
         self.builder.call(printf, [cstr])
 
     def finish(self):
-        self.builder.ret(ir.IntType(32)(0))
+        """Return 0 and emit the final LLVM IR"""
+        if self.builder.block.is_terminated is False:
+            self.builder.ret(ir.IntType(32)(0))
         return str(self.module)
