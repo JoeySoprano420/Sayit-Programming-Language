9fb36fd2a6a90f9cb13c0d07e671774b9b0dfeb2
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2025-09-13 01:23:13 -0400

    Implement main function for sayc script

--- sayc.py
+++ sayc.py
@@ -17,3 +17,33 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+import sys
+from say_lexer import tokenize
+from say_parser import Parser
+from say_vm import VM
+from say_codegen import Codegen
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: sayc <file.say> [--ir]")
+        return
+
+    code = open(sys.argv[1]).read()
+    tokens = tokenize(code)
+    parser = Parser(tokens)
+    program = parser.parse_program()
+
+    if "--ir" in sys.argv:
+        cg = Codegen()
+        cg.emit_main()
+        for stmt in program.stmts:
+            if stmt.__class__.__name__ == "Print":
+                cg.emit_print(stmt.expr.val)
+        print(cg.finish())
+    else:
+        vm = VM()
+        vm.run(program)
+
+if __name__ == "__main__":
+    main()
